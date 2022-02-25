fc1bfe1b5c6453b540678aa6e648546a2bfa3d60
Burton Parker <burtonparker@gmail.com>
2022-02-25 09:01:15 -0600

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express'); // no file path 'cus this is a core module dude
-
+const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express(); // calls the express function which gives us a server, now we can call it using 'app'
 
+app.use(morgan('dev')); // dev causes morgan to log using the development version, which logs additional info to the screen
+
+app.use(express.static(__dirname + '/public')); // __dirname is a special variable in node, when used it refers to the absolute path of the current directory of the file that it's in
+
 app.use((req, res) => {
-    console.log(req.headers);
+    // console.log(req.headers); // removed because morgan handles this for us now
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
